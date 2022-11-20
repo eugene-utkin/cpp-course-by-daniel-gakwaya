9777994a01bf303cab68b9173d62f199ba56b8a6
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-11-20 17:06:00 +0600

    started c++ course

---
+++ 3.2FirstCppProgram/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+
+    std::cout << "Evgeniy1" << std::endl;
+    std::cout << "Evgeniy2" << std::endl;
+    std::cout << "Evgeniy3" << std::endl;
+    std::cout << "Evgeniy4" << std::endl;
+    std::cout << "Evgeniy5" << std::endl;
+    std::cout << "Evgeniy6" << std::endl;
+    std::cout << "Evgeniy7" << std::endl;
+    std::cout << "Evgeniy8" << std::endl;
+    std::cout << "Evgeniy9" << std::endl;
+    std::cout << "Evgeniy10" << std::endl;
+
+    return 0;
+}
